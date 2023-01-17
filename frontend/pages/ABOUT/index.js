309b79bb965988ea09c7cdca2fba6daea88f447d
Trishant Kumar Talluri <118588406+TRISHANT131104@users.noreply.github.com>
2023-01-17 11:14:06 +0530

    Update index.js

--- frontend/pages/ABOUT/index.js
+++ frontend/pages/ABOUT/index.js
@@ -3,7 +3,258 @@ import React from 'react'
 export default function index() {
   return (
     <div>
-      {/* add your about us page content here */}
+      {    <section class="text-gray-600 body-font overflow-hidden">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="lg:w-[70%] mx-auto flex flex-wrap">
+            <img alt="ecommerce" class="lg:w-1/3 w-full lg:h-auto h-64 object-contain object-center rounded " src="./pngtree-programming-and-web-technology-vector-with-computer-illustration-png-image_755051.jpg">
+            <div class="lg:w-2/3 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">About Us</h1>
+              <div class="flex mb-4">
+                <span class="flex items-center">
+                  <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                </span>
+              </div>
+                <p class="leading-relaxed text-lg text-black">This is a website based on the concept of web-scrapping, it scrapes useful  data from popular websites on competetive pogramming and the collection of all those data is shown on our webiste. Anyone can access these competetions form our website. Our main objective is to make it easier for user to access different competetions.
+                </p>
+            </div>
+          </div>
+        </div>
+    </section>
+
+
+
+
+<section class="text-gray-600 body-font">
+        <div class="container px-5 py-12 mx-auto">
+
+          <div class="flex flex-col text-center w-full mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
+          </div>
+
+            <div>
+                <div>
+                  <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Web scrappring team</h1>
+                </div>
+                <div class="flex flex-wrap -m-2">
+                  <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="./Akhilesh_Panchangam.jpg">
+                      <div class="flex-grow">
+                        <h2 class="text-gray-900 title-font font-medium">Akhilesh panchangam</h2>
+                        <p class="text-gray-500">Team Lead</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84">
+                      <div class="flex-grow">
+                        <h2 class="text-gray-900 title-font font-medium">Darshil Patel</h2>
+                        <p class="text-gray-500">Member</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88">
+                      <div class="flex-grow">
+                        <h2 class="text-gray-900 title-font font-medium">Manas</h2>
+                        <p class="text-gray-500">Member</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84">
+                      <div class="flex-grow">
+                        <h2 class="text-gray-900 title-font font-medium">Shivam Sundaram</h2>
+                        <p class="text-gray-500">Member</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                    <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                      <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88">
+                      <div class="flex-grow">
+                        <h2 class="text-gray-900 title-font font-medium">Ayush</h2>
+                        <p class="text-gray-500">Member</p>
+                      </div>
+                    </div>
+                  </div>
+            </div>                  
+        </div>
+    </div>
+  </section>
+</section>
+
+
+<section class="text-gray-600 body-font">
+    <div class="container px-5 py-12 mx-auto">
+        <div>
+            <div>
+              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Debugging team</h1>
+            </div>
+            <div class="flex flex-wrap -m-2">
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Garima Upadhyay</h2>
+                    <p class="text-gray-500">Team Lead</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="./Suhana_Shaik.jpg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Suhana Shaik</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium"></h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+        </div>                  
+    </div>
+</div>
+</div>
+</section>
+
+
+<section class="text-gray-600 body-font">
+    <div class="container px-5 py-12 mx-auto">
+        <div>
+            <div>
+              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Web developing team</h1>
+            </div>
+            <div class="flex flex-wrap -m-2">
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="./Pradeep_kumar_rebbavarpu.jpg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Pradeep Kumar Rebbavarapu</h2>
+                    <p class="text-gray-500">Team Lead</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="./Trishant_Talluri.jpg">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Trishant Kumar Talluri</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">P C Uma Mahesh</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Vashishta Narayan</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Chiranjivi Keshav</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Aditya Kshitiz</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Rithvik</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Samyak</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Niketh</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Aviral Sharma</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88">
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">Srujan</h2>
+                    <p class="text-gray-500">Member</p>
+                  </div>
+                </div>
+              </div>
+        </div>                  
+    </div>
+</div>
+</div>
+</section>}
     </div>
   )
 }
